200295b5c87e7e45dc9c7b9a008653f00e339c25
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-12 17:23:06 +0300

    Adding application.c file

---
+++ application.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   application.c
+ * Author: ssmm
+ *
+ * Created on September 12, 2024, 5:17 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
