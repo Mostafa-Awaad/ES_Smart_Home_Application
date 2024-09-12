148b25f88525673390726a5d32eb553ec630fde5
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-12 18:54:00 +0300

    Adding Microcontroller Abstraction Layer

---
+++ MCAL_Layer/hal_my_gpio.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   hal_my_gpio.h
+ * Author: Mostafa Mohamed Kamal
+ *
+ * Created on September 12, 2024, 6:52 PM
+ */
+
+#ifndef HAL_MY_GPIO_H
+#define	HAL_MY_GPIO_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HAL_MY_GPIO_H */
+
