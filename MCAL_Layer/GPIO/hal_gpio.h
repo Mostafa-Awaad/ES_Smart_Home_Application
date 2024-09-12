4720d2b08942d3d97b27eac2c13a63f3569241f3
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-09-13 00:45:20 +0300

    adding hal_gpio header file and modifying hal_gpio source file in the GPIO SW Module

---
+++ MCAL_Layer/GPIO/hal_gpio.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   hal_gpio.h
+ * Author: ssmm
+ *
+ * Created on September 13, 2024, 12:41 AM
+ */
+
+#ifndef HAL_GPIO_H
+#define	HAL_GPIO_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HAL_GPIO_H */
+
--- MCAL_Layer/GPIO/hal_my_gpio.c
+++ MCAL_Layer/GPIO/hal_my_gpio.c
@@ -1,1 +1,6 @@
-
+/* 
+ * File:   hal_my_gpio.c
+ * Author: Mostafa Mohamed Kamal
+ *
+ * Created on September 12, 2024, 5:17 PM
+ */
